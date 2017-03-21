88266f52d54b65e4d676ccac2001813f15d18aff
Florian Polster <fpolster@uni-koblenz.de>
2017-03-21 16:20:33 +0100

    clang format

--- src/ParticleFilter/HyperSlamFilter.cpp
+++ src/ParticleFilter/HyperSlamFilter.cpp
@@ -1,36 +1,36 @@
 #include <homer_mapping/ParticleFilter/HyperSlamFilter.h>
 
-#include <vector>
+#include <stdlib.h>
 #include <cmath>
 #include <fstream>
 #include <sstream>
-#include <stdlib.h>
+#include <vector>
 
 #include "ros/ros.h"
 
 using namespace std;
 
-HyperSlamFilter::HyperSlamFilter (int particleFilterNum, int particleNum )
+HyperSlamFilter::HyperSlamFilter(int particleFilterNum, int particleNum)
 {
   m_ParticleFilterNum = particleFilterNum;
-        if ( m_ParticleFilterNum < 1 )
-        {
-                m_ParticleFilterNum = 1;
-        }
-  ROS_DEBUG( "Using %d Hyper Particles.", particleFilterNum);
+  if (m_ParticleFilterNum < 1)
+  {
+    m_ParticleFilterNum = 1;
+  }
+  ROS_DEBUG("Using %d Hyper Particles.", particleFilterNum);
 
   m_ParticleNum = particleNum;
 
-        m_DoMapping = true;
+  m_DoMapping = true;
 
   m_DeletionThreshold = 0.98;
 
-  for ( unsigned i=0; i < m_ParticleFilterNum; i++ )
+  for (unsigned i = 0; i < m_ParticleFilterNum; i++)
   {
     ostringstream stream;
     stream << "SlamFilter " << i;
-    SlamFilter *slamFilter = new SlamFilter ( particleNum );
-    m_SlamFilters.push_back ( slamFilter );
+    SlamFilter* slamFilter = new SlamFilter(particleNum);
+    m_SlamFilters.push_back(slamFilter);
   }
 
   m_BestSlamFilter = m_SlamFilters[0];
@@ -40,7 +40,7 @@ HyperSlamFilter::~HyperSlamFilter()
 {
   for (unsigned i = 0; i < m_ParticleFilterNum; i++)
   {
-    if( m_SlamFilters[i] )
+    if (m_SlamFilters[i])
     {
       delete m_SlamFilters[i];
       m_SlamFilters[i] = 0;
@@ -48,139 +48,144 @@ HyperSlamFilter::~HyperSlamFilter()
   }
 }
 
-void HyperSlamFilter::setRotationErrorRotating ( float percent )
+void HyperSlamFilter::setRotationErrorRotating(float percent)
 {
-   for ( unsigned i=0; i < m_SlamFilters.size(); i++ )
-   {
-     m_SlamFilters[i]->setRotationErrorRotating(percent / 100.0);
-   }
-}
-
-void HyperSlamFilter::setRotationErrorTranslating ( float degreePerMeter )
-{
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned i = 0; i < m_SlamFilters.size(); i++)
   {
-    m_SlamFilters[i]->setRotationErrorTranslating(degreePerMeter / 180.0 * M_PI);
+    m_SlamFilters[i]->setRotationErrorRotating(percent / 100.0);
   }
 }
 
-void HyperSlamFilter::setTranslationErrorTranslating ( float percent )
+void HyperSlamFilter::setRotationErrorTranslating(float degreePerMeter)
 {
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
+  {
+    m_SlamFilters[i]->setRotationErrorTranslating(degreePerMeter / 180.0 *
+                                                  M_PI);
+  }
+}
+
+void HyperSlamFilter::setTranslationErrorTranslating(float percent)
+{
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
     m_SlamFilters[i]->setTranslationErrorTranslating(percent / 100.0);
   }
 }
 
-void HyperSlamFilter::setTranslationErrorRotating ( float mPerDegree )
+void HyperSlamFilter::setTranslationErrorRotating(float mPerDegree)
 {
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
-    m_SlamFilters[i]->setTranslationErrorRotating( mPerDegree / 180.0 * M_PI );
+    m_SlamFilters[i]->setTranslationErrorRotating(mPerDegree / 180.0 * M_PI);
   }
 }
 
-void HyperSlamFilter::setMoveJitterWhileTurning ( float mPerDegree )
+void HyperSlamFilter::setMoveJitterWhileTurning(float mPerDegree)
 {
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
-    m_SlamFilters[i]->setMoveJitterWhileTurning( mPerDegree / 180.0 * M_PI );
+    m_SlamFilters[i]->setMoveJitterWhileTurning(mPerDegree / 180.0 * M_PI);
   }
 }
 
 void HyperSlamFilter::resetHigh()
 {
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
     m_SlamFilters[i]->resetHigh();
   }
 }
 
-void HyperSlamFilter::setMapping ( bool doMapping )
+void HyperSlamFilter::setMapping(bool doMapping)
 {
-    m_DoMapping = doMapping;
+  m_DoMapping = doMapping;
 }
 
-void HyperSlamFilter:: setOccupancyMap ( OccupancyMap* occupancyMap )
+void HyperSlamFilter::setOccupancyMap(OccupancyMap* occupancyMap)
 {
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
-    m_SlamFilters[i]->setOccupancyMap( occupancyMap );
+    m_SlamFilters[i]->setOccupancyMap(occupancyMap);
   }
 }
 
-void HyperSlamFilter::setRobotPose ( Pose pose, double scatterVarXY, double scatterVarTheta )
+void HyperSlamFilter::setRobotPose(Pose pose, double scatterVarXY,
+                                   double scatterVarTheta)
 {
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
     m_SlamFilters[i]->setRobotPose(pose, scatterVarXY, scatterVarTheta);
   }
 }
 
-void HyperSlamFilter::filter ( Transformation2D trans, sensor_msgs::LaserScanConstPtr laserData)
+void HyperSlamFilter::filter(Transformation2D trans,
+                             sensor_msgs::LaserScanConstPtr laserData)
 {
-  //call filter methods of all particle filters
-  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
+  // call filter methods of all particle filters
+  for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
   {
     bool randOnOff;
 
-	if(m_SlamFilters.size() == 1)
-	{
-		randOnOff = true;
-	}
-	else
-	{
-		//if mapping is on, switch on with 80% probability to introduce some randomness in different particle filters
-		randOnOff = (rand() % 100) < 80;
-	}
-    m_SlamFilters[i]->setMapping( m_DoMapping && randOnOff );
+    if (m_SlamFilters.size() == 1)
+    {
+      randOnOff = true;
+    }
+    else
+    {
+      // if mapping is on, switch on with 80% probability to introduce some
+      // randomness in different particle filters
+      randOnOff = (rand() % 100) < 80;
+    }
+    m_SlamFilters[i]->setMapping(m_DoMapping && randOnOff);
     m_SlamFilters[i]->filter(trans, laserData);
-	   	
   }
-  if(m_SlamFilters.size() != 1)
+  if (m_SlamFilters.size() != 1)
   {
-	  //determine which map has the best/worst contrast
-	  double bestContrast = 0.0;
-	  static unsigned int bestFilter = 0;
-	  double worstContrast = 100.0;
-	  static unsigned int worstFilter = 0;
+    // determine which map has the best/worst contrast
+    double bestContrast = 0.0;
+    static unsigned int bestFilter = 0;
+    double worstContrast = 100.0;
+    static unsigned int worstFilter = 0;
 
-	  for ( unsigned int i=0; i < m_SlamFilters.size(); i++ )
-	  {
-		double contrast = m_SlamFilters[i]->evaluateByContrast();
-		            {
-		                    if( contrast > bestContrast )
-		                    {
-		                            bestContrast = contrast;
-		                            bestFilter = i;
-		                    }
-		                    if ( contrast < worstContrast )
-		                    {
-		                            worstContrast = contrast;
-		                            worstFilter = i;
-		                    }
-		            }
-	  }
+    for (unsigned int i = 0; i < m_SlamFilters.size(); i++)
+    {
+      double contrast = m_SlamFilters[i]->evaluateByContrast();
+      {
+        if (contrast > bestContrast)
+        {
+          bestContrast = contrast;
+          bestFilter = i;
+        }
+        if (contrast < worstContrast)
+        {
+          worstContrast = contrast;
+          worstFilter = i;
+        }
+      }
+    }
 
-	  // set best filter
-	  SlamFilter* lastBestFilter = m_BestSlamFilter;
-	  m_BestSlamFilter = m_SlamFilters[bestFilter];
+    // set best filter
+    SlamFilter* lastBestFilter = m_BestSlamFilter;
+    m_BestSlamFilter = m_SlamFilters[bestFilter];
 
-	  if ( m_BestSlamFilter != lastBestFilter )
-	  {
-		ROS_INFO( "Switched to best filter %d (bestContrast: %f) -- the worst filter is %d (worstContrast: %f)", bestFilter, bestContrast, worstFilter, worstContrast); //TODO
-	  }
+    if (m_BestSlamFilter != lastBestFilter)
+    {
+      ROS_INFO("Switched to best filter %d (bestContrast: %f) -- the worst "
+               "filter is %d (worstContrast: %f)",
+               bestFilter, bestContrast, worstFilter, worstContrast);  // TODO
+    }
 
-	  if ( bestFilter != worstFilter )
-	  {
-		if ( worstContrast < ( bestContrast * m_DeletionThreshold ) )
-		{
-		  // replace the worst filter by the one with the best contrast
-		  delete m_SlamFilters[worstFilter];
-		  m_SlamFilters[worstFilter] = new SlamFilter ( * m_SlamFilters [bestFilter] );
-		}
-	  }
-   }
+    if (bestFilter != worstFilter)
+    {
+      if (worstContrast < (bestContrast * m_DeletionThreshold))
+      {
+        // replace the worst filter by the one with the best contrast
+        delete m_SlamFilters[worstFilter];
+        m_SlamFilters[worstFilter] = new SlamFilter(*m_SlamFilters[bestFilter]);
+      }
+    }
+  }
 }
 
 SlamFilter* HyperSlamFilter::getBestSlamFilter()
--- src/ParticleFilter/SlamParticle.cpp
+++ src/ParticleFilter/SlamParticle.cpp
@@ -1,41 +1,47 @@
 #include <homer_mapping/ParticleFilter/SlamParticle.h>
 
-SlamParticle::SlamParticle(float weight, float robotX, float robotY, float robotTheta) : Particle(weight) {
+SlamParticle::SlamParticle(float weight, float robotX, float robotY,
+                           float robotTheta)
+  : Particle(weight)
+{
   m_RobotPositionX = robotX;
   m_RobotPositionY = robotY;
   m_RobotOrientation = robotTheta;
 }
 
-SlamParticle::SlamParticle( SlamParticle& slamParticle )
+SlamParticle::SlamParticle(SlamParticle& slamParticle)
 {
   m_RobotPositionX = slamParticle.m_RobotPositionX;
   m_RobotPositionY = slamParticle.m_RobotPositionY;
   m_RobotOrientation = slamParticle.m_RobotOrientation;
 }
 
-SlamParticle::~SlamParticle() {
+SlamParticle::~SlamParticle()
+{
 }
 
-void SlamParticle::setRobotPose(float robotX, float robotY, float robotTheta) {
+void SlamParticle::setRobotPose(float robotX, float robotY, float robotTheta)
+{
   m_RobotPositionX = robotX;
   m_RobotPositionY = robotY;
   m_RobotOrientation = robotTheta;
 }
 
-void SlamParticle::setRobotPose(Pose pose){
-    m_RobotPositionX = pose.x();
-    m_RobotPositionY = pose.y();
-    m_RobotOrientation = pose.theta();
+void SlamParticle::setRobotPose(Pose pose)
+{
+  m_RobotPositionX = pose.x();
+  m_RobotPositionY = pose.y();
+  m_RobotOrientation = pose.theta();
 }
 
-void SlamParticle::getRobotPose(float& robotX, float& robotY, float& robotTheta) {
+void SlamParticle::getRobotPose(float& robotX, float& robotY, float& robotTheta)
+{
   robotX = m_RobotPositionX;
   robotY = m_RobotPositionY;
   robotTheta = m_RobotOrientation;
 }
 
-Pose SlamParticle::getRobotPose(){
-    return Pose(m_RobotPositionX, m_RobotPositionY, m_RobotOrientation);
+Pose SlamParticle::getRobotPose()
+{
+  return Pose(m_RobotPositionX, m_RobotPositionY, m_RobotOrientation);
 }
-
-
